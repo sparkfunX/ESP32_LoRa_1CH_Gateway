7d770771ecad70f944a9cab24060692081054c84
jimblom <jim.lindblom@sparkfun.com>
2018-10-24 09:17:16 -0600

    Modifying and posting test code. Adding receiver test code.

---
+++ Production/Receiver/esp32_lora_receiver.bat
@@ -0,0 +1,5 @@
+@set ARDUINO_SKETCHBOOK=C:\Users\jim.lindblom\Dropbox\SparkFun\Arduino
+@set ESPTOOL=%ARDUINO_SKETCHBOOK%\hardware\espressif\esp32/tools/esptool.exe
+@set COM_PORT=COM3
+
+%ESPTOOL% --chip esp32 --port %COM_PORT% --baud 921600 --before default_reset --after hard_reset write_flash -z --flash_mode dio --flash_freq 80m --flash_size detect 0xe000 boot_app0.bin 0x1000 bootloader_qio_80m.bin 0x10000 ESP32_Lora_Receiver_sketch.ino.bin 0x8000 ESP32_Lora_Receiver_sketch.ino.partitions.bin 
